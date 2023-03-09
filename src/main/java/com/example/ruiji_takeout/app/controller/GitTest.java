c413c3b2191260f29f288cfe3b0d7e3aa56e83d7
layne <928814697@qq.com>
2023-03-09 22:58:13 +0800

    reggie-v1

---
+++ src/main/java/com/example/ruiji_takeout/app/controller/GitTest.java
@@ -0,0 +1,16 @@
+package com.example.ruiji_takeout.app.controller;
+
+import org.junit.Test;
+
+/**
+ * @author Jmd
+ * @create 2023-03-2023/3/9-22:56
+ * @Description：
+ */
+public class GitTest {
+
+    @Test
+    public void test(){
+        System.out.println("v2");
+    }
+}
